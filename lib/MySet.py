03f48b72b8d1bad29434a4f1b8cfd218489f2116
ASHFAQ ISSAK <ashfaq.abdi@gmail.com>
2023-06-07 09:34:10 +0300

    completed  assignment

--- lib/MySet.py
+++ lib/MySet.py
@@ -1,3 +1,19 @@
 class MySet:
+    def __init__(self, initial_set=None):
+        self.dictionary = {}
+        if initial_set is not None:
+            for item in initial_set:
+                self.dictionary[item] = True
 
-    pass
+    def add(self, item):
+        self.dictionary[item] = True
+
+    def delete(self, item):
+        if item in self.dictionary:
+            del self.dictionary[item]
+
+    def has(self, item):
+        return item in self.dictionary
+
+    def size(self):
+        return len(self.dictionary)
